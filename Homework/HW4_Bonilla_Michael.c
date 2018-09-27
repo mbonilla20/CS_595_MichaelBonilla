682d3105ee1da3d132c6bccb7b2f6e50a2d79534
Michael Bonilla <mbonilla@hawk.iit.edu>
2018-09-27 14:15:16 -0500

    HW4 attempt 3

--- Homework/HW4_Bonilla_Michael.c
+++ Homework/HW4_Bonilla_Michael.c
@@ -35,7 +35,7 @@ int main(int argc,char **args)
   KSP            ksp;      /* linear solver context */
   PetscRandom    rctx;     /* random number generator context */
   PetscReal      norm;     /* norm of solution error */
-  PetscInt       i,j,Ii,J,Istart,Iend,m = 8,n = 7,its,N,rstart,rend;
+  PetscInt       i,j,Ii,J,Istart,Iend,m = 8,n = 7,its,N,rstart,rend,ng;
   PetscErrorCode ierr;
   PetscBool      flg = PETSC_FALSE;
   PetscScalar    v,one=1.0;
