be231103c272b590d5cc7e13fecd2b73d78d8104
mbonillaiit <mbonilla@hawk.iit.edu>
2018-11-27 22:02:26 -0600

    Slide Updates

--- project/kexample.m
+++ project/kexample.m
@@ -4,23 +4,23 @@ f1 = @(normt,a,d) ((2*pi*a^2).^(d/2)) * cos(a*sqrt(normt)) ...
 f = @(t,a,d) f1(normsqd(t),a,d);
 abstol = 0; %absolute error tolerance
 reltol = 0.01; %relative error tolerance
-dvec = 250; %vector of dimensions
+dvec = [1:10,10:10:100,150:50:300]'; %vector of dimensions
 a = 1; %default value of a
 a = 1; %default value of a again
 cpuq = zeros(size(dvec)); %vector of answers
+cputime=cpuq;
+gputime=cpuq;
+for i=1:size(dvec,1)
 tic();
-for d = dvec
-   cpuq(d) = cubLattice_g(@(x) f(x,a,d),[-inf(1,d); inf(1,d)], ...
+   cpuq(i) = cubLattice_g(@(x) f(x,a,dvec(i)),[-inf(1,dvec(i)); inf(1,dvec(i))], ...
       'normal',abstol,reltol);
-end
-cputime=toc();
-
+cputime(i)=toc();
+gpuDevice(1);
 gpuq = zeros(size(dvec)); %vector of answers
 tic();
-for d = dvec
-   gpuq(d) = cubLattice_g_GPU1(@(x) f(x,a,d),[-inf(1,d); inf(1,d)], ...
+   gpuq(i) = cubLattice_g_GPU1(@(x)f(x,a,dvec(i)),[-inf(1,dvec(i)); inf(1,dvec(i))], ...
       'normal',abstol,reltol);
+gputime(i)=toc();
+gpuDevice(1);
 end
-gputime=toc();
-
 
